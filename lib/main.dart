26948b7f827ed88d790f7ffff1cc1ebeb9e36dbf
zahid2873 <mahdi.zhm13@gmail.com>
2023-07-28 21:44:05 +0600

    detail page

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context)=>NewsProvider()),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
          
--- lib/screen/news_details.dart
+++ lib/screen/news_details.dart
@@ -18,10 +18,43 @@ class NewsDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("${articles!.source!.name}"),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: size.height *.3,
+              width: double.infinity,
+              child: articles!.urlToImage==null? Center(child: CircularProgressIndicator(),):
+              Image.network("${articles!.urlToImage}",fit: BoxFit.cover,),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text("${articles!.title}", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Icon(Icons.newspaper),
+                  Text("${articles!.author}"),
+                  Spacer(),
+                  Icon(Icons.calendar_month_sharp),
+                  Text("${articles!.publishedAt}"),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text("${articles!.description}"),
+            ),
+          ],
+        ),
+      ),
     );
-  }
+   }
 }
